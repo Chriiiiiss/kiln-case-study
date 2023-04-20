d8b48c6bdb528e71681cf50ca36491754f544727
Chris <chris.fraanc@gmail.com>
2023-04-20 21:31:59 +0200

    [ADD] Functional Reward Calculator

--- components/RewardComponent.tsx
+++ components/RewardComponent.tsx
@@ -1,12 +1,19 @@
+import { formatNumber, formatPrice } from "@/src/utils";
 interface RewardComponentProps {
   ETHPrice: number;
   inputValue: number;
 }
 
+const calcETHYearlyReward = (inputValue: number) => {
+  return (inputValue * 5) / 100;
+};
+
 const RewardComponent: React.FC<RewardComponentProps> = ({
   ETHPrice,
   inputValue,
 }) => {
+  let YearlyETHReward = calcETHYearlyReward(inputValue);
+
   return (
     <div className="text-white flex flex-col gap-y-8">
       <p>
@@ -14,30 +21,36 @@ const RewardComponent: React.FC<RewardComponentProps> = ({
       </p>
       <div className="flex justify-between">
         <span>Daily</span>
-        <span>
+        <div className="flex gap-x-2">
+          <span>{formatNumber(YearlyETHReward / 365)}</span>
           <span>ETH</span>
-        </span>
-        <span>
+        </div>
+        <div className="flex gap-x-2">
+          <span>{formatPrice((YearlyETHReward / 365) * ETHPrice)}</span>
           <span>$</span>
-        </span>
+        </div>
       </div>
       <div className="flex justify-between">
         <span>Monthly</span>
-        <span>
+        <div className="flex gap-x-2">
+          <span>{formatNumber(YearlyETHReward / 12)}</span>
           <span>ETH</span>
-        </span>
-        <span>
+        </div>
+        <div className="flex gap-x-2">
+          <span>{formatPrice((YearlyETHReward / 12) * ETHPrice)}</span>
           <span>$</span>
-        </span>
+        </div>
       </div>
       <div className="flex justify-between">
         <span>Yearly</span>
-        <span>
+        <div className="flex gap-x-2">
+          <span>{formatNumber(YearlyETHReward)}</span>
           <span>ETH</span>
-        </span>
-        <span>
+        </div>
+        <div className="flex gap-x-2">
+          <span>{formatPrice(YearlyETHReward * ETHPrice)}</span>
           <span>$</span>
-        </span>
+        </div>
       </div>
     </div>
   );
